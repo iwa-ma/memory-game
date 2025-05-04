6f40c743eb400c2634c50829f8624966b3bcf4b1
iwa-ma <iwakura31@gmail.com>
2025-05-04 19:44:32 +0900

    [スタート待機] refactor : スタイル設定を別ファイルに分離

---
+++ src/styles/NoticeStyles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+/** 音声注意文のスタイル */ 
+export const SoundNotice = styled.p`
+  font-size: 0.9em;
+  color: #61dafb;
+  margin-top: 0.5rem;
+  margin-bottom: 1.5rem;
+`; 
